c5c86448a9154e4837bcbcf058294b034a3b9cc8
zhiqiang.fan <zhiqiang.fan@pintec.com>
2019-08-23 22:12:59 +0800

    fix

--- nacossync-worker/src/main/java/com/alibaba/nacossync/extension/impl/EurekaSyncToNacosServiceImpl.java
+++ nacossync-worker/src/main/java/com/alibaba/nacossync/extension/impl/EurekaSyncToNacosServiceImpl.java
@@ -12,6 +12,7 @@
  */
 package com.alibaba.nacossync.extension.impl;
 
+import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 import com.alibaba.nacossync.cache.SkyWalkerCacheServices;
@@ -33,10 +34,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * eureka
@@ -58,6 +57,8 @@ public class EurekaSyncToNacosServiceImpl implements SyncService {
 
     private final SpecialSyncEventBus specialSyncEventBus;
 
+    private final LinkedBlockingQueue<Map<String, List<InstanceInfo>>> registeredInstance = new LinkedBlockingQueue<>();
+
     @Autowired
     public EurekaSyncToNacosServiceImpl(EurekaServerHolder eurekaServerHolder, SkyWalkerCacheServices skyWalkerCacheServices, NacosServerHolder nacosServerHolder, SpecialSyncEventBus specialSyncEventBus) {
         this.eurekaServerHolder = eurekaServerHolder;
@@ -71,7 +72,7 @@ public class EurekaSyncToNacosServiceImpl implements SyncService {
 
         try {
             specialSyncEventBus.unsubscribe(taskDO);
-            NamingService destNamingService = nacosServerHolder.get(taskDO.getDestClusterId(), null);
+            NamingService destNamingService = nacosServerHolder.get(taskDO.getDestClusterId(), taskDO.getNameSpace());
             List<Instance> allInstances = destNamingService.getAllInstances(taskDO.getServiceName());
             for (Instance instance : allInstances) {
                 if (needDelete(instance.getMetadata(), taskDO)) {
@@ -89,31 +90,52 @@ public class EurekaSyncToNacosServiceImpl implements SyncService {
 
     @Override
     public boolean sync(TaskDO taskDO) {
+        NamingService destNamingService = null;
         try {
-            EurekaHttpClient eurekaHttpClient = eurekaServerHolder.get(taskDO.getSourceClusterId(), null);
-            NamingService destNamingService = nacosServerHolder.get(taskDO.getDestClusterId(), null);
+            EurekaHttpClient eurekaHttpClient = eurekaServerHolder.get(taskDO.getSourceClusterId(), taskDO.getGroupName());
+            destNamingService = nacosServerHolder.get(taskDO.getDestClusterId(), taskDO.getGroupName());
             EurekaHttpResponse<Application> eurekaHttpResponse =
-                eurekaHttpClient.getApplication(taskDO.getServiceName());
+                    eurekaHttpClient.getApplication(taskDO.getServiceName());
             if (Objects.requireNonNull(HttpStatus.resolve(eurekaHttpResponse.getStatusCode())).is2xxSuccessful()) {
                 Application application = eurekaHttpResponse.getEntity();
+                ArrayList<InstanceInfo> objects = new ArrayList<>();
                 for (InstanceInfo instanceInfo : application.getInstances()) {
                     if (needSync(instanceInfo.getMetadata())) {
                         if (InstanceInfo.InstanceStatus.UP.equals(instanceInfo.getStatus())) {
                             destNamingService.registerInstance(taskDO.getServiceName(),
-                                buildSyncInstance(instanceInfo, taskDO));
+                                    buildSyncInstance(instanceInfo, taskDO));
+                            objects.add(instanceInfo);
                         } else {
                             destNamingService.deregisterInstance(instanceInfo.getAppName(), instanceInfo.getIPAddr(),
-                                instanceInfo.getPort());
+                                    instanceInfo.getPort());
                         }
                     }
-
+                }
+                Map<String, List<InstanceInfo>> peek = registeredInstance.peek();
+                if (peek == null || peek.get(taskDO.getServiceName()) == null) {
+                    HashMap<String, List<InstanceInfo>> objectObjectHashMap = new HashMap<>();
+                    objectObjectHashMap.put(taskDO.getServiceName(), objects);
+                    registeredInstance.add(objectObjectHashMap);
                 }
             } else {
                 metricsManager.recordError(MetricsStatisticsType.SYNC_ERROR);
-                throw new RuntimeException("trying to connect to the server failed");
+                throw new RuntimeException("trying to connect to the server failed" + taskDO);
             }
             specialSyncEventBus.subscribe(taskDO, this::sync);
         } catch (Exception e) {
+            Map<String, List<InstanceInfo>> poll = registeredInstance.poll();
+            if (poll != null && destNamingService != null) {
+                List<InstanceInfo> instanceInfos = poll.get(taskDO.getServiceName());
+                for (InstanceInfo instanceInfo : instanceInfos) {
+                    try {
+                        destNamingService.deregisterInstance(taskDO.getServiceName(), instanceInfo.getIPAddr(),
+                                instanceInfo.getPort());
+                    } catch (Exception e1) {
+                        return false;
+                    }
+                    return true;
+                }
+            }
             log.error("sync task from eureka to nacos was failed, taskId:{}", taskDO.getTaskId(), e);
             metricsManager.recordError(MetricsStatisticsType.SYNC_ERROR);
             return false;
@@ -125,7 +147,7 @@ public class EurekaSyncToNacosServiceImpl implements SyncService {
         Instance temp = new Instance();
         temp.setIp(instance.getIPAddr());
         temp.setPort(instance.getPort());
-        temp.setServiceName(instance.getAppName());
+        temp.setServiceName(instance.getAppName().toLowerCase());
         temp.setHealthy(true);
 
         Map<String, String> metaData = new HashMap<>();
